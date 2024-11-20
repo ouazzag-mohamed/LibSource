0d3c94be2ad49c548821183db168ef91a48b2c79
therealfianso <125743052+therealfianso@users.noreply.github.com>
2024-11-20 23:26:31 +0100

    swiper cards add , problem too big

--- geni informatique/script.js
+++ geni informatique/script.js
@@ -1,29 +1,28 @@
-new Swiper('.card-wrapper', {
-
+new Swiper('.swiper', {
     loop: true,
-    spaceBetween: 30,
+    spaceBetween: 15, // Reduced space between slides
+    slidesPerView: 1, // Default for mobile
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+    breakpoints: {
+      640: {
+        slidesPerView: 2, // Two cards for small screens
+      },
+      768: {
+        slidesPerView: 3, // Three cards for tablets
+      },
+      1024: {
+        slidesPerView: 4, // Four cards for desktops
+      },
     },
   
-    // Navigation arrows
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
-    breakpoint:{
-        0:{
-            slidesPreView: 1
-        },
-        768: {
-            slidesPreView: 2
-        },
-        1024: {
-            slidesPreView : 3
-        },
-    }
-  
   });
+  
